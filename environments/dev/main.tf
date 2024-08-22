a85e1ad12af021bec4840635347a72674b909cc3
Dinush Chathurya <dinushchathurya21@gmail.com>
2024-08-22 22:55:33 +0100

    feat: Update script files

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,8 @@
 module "vpc_dev" {
     source   = "../../modules/vpc"
     cidr_block = var.dev_vpc_cidr_block
-    env_name   = "dev"
+    env_name   = var.dev_vpc_env_name
     tags = {
-        "Environment" = "Development"
+        "Environment" = var.dev_vpc_env_name
     }
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,3 +2,9 @@ variable "dev_vpc_cidr_block" {
     description = "CIDR block for the development VPC"
     type        = string
 }
+
+variable "dev_vpc_env_name" {
+    description = "Name of the development environment"
+    type        = string
+    default = "dev"
+}
