10e89752bd63ca8541377ecf6750a50b0e8b0ad3
Dinush Chathurya <dinushchathurya21@gmail.com>
2024-08-25 21:07:22 +0100

    feat: Add variables related to subnet
    Signed-off-by: Dinush Chathurya <dinushchathurya21@gmail.com>

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -13,3 +13,20 @@ variable "vpc_requester_env_name" {
     type        = string
     default = "dev"
 }
+
+variable "subnet_requester_cidr_block" {
+    type        = string
+    description = "CIDR block for Subnet 1"
+}
+
+variable "subnet_requester_availability_zone" {
+    type        = string
+    description = "Availability zone for Subnet 1"
+    default = "us-west-2a"
+}
+
+variable "subnet_requester_env_name" {
+    type        = string
+    description = "Name of the development environment"
+    default = "dev"
+}
